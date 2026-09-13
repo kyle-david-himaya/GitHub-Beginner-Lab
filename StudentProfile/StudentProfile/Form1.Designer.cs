13fae43afed754186dda50facd78cd65b6ada831
kyle-david-himaya <Kylehimaya123@gmail.com>
2026-09-13 23:06:24 +0800

    Implement student email (#2)

--- StudentProfile/StudentProfile/Form1.Designer.cs
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label
@@ -52,11 +53,22 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Contact Number: 09512826283";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email: student@school.edu";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(334, 117);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label);
             this.Name = "Form1";
@@ -70,6 +82,7 @@
 
         private System.Windows.Forms.Label label;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
