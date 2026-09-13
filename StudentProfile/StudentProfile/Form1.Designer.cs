3613de19b5dbc52ad52cd1b81b9d64a1d04b01aa
kyle-david-himaya <Kylehimaya123@gmail.com>
2026-09-13 21:43:01 +0800

    Adjust student contact number position

--- StudentProfile/StudentProfile/Form1.Designer.cs
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.label.AutoSize = true;
             this.label.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label.Location = new System.Drawing.Point(1, 9);
+            this.label.Location = new System.Drawing.Point(12, 9);
             this.label.Name = "label";
             this.label.Size = new System.Drawing.Size(244, 20);
             this.label.TabIndex = 0;
@@ -46,7 +46,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(1, 51);
+            this.label1.Location = new System.Drawing.Point(12, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(194, 20);
             this.label1.TabIndex = 1;
